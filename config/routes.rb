0789685125cfcaacaa4d24c0173180c9be307144
phonghoang@gmail.com <phonghoang@gmail.com>
2015-09-20 21:09:06 -0700

    added a new route for creatures

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-    root to: 'creatures#index' # same as `get "/" => "creatures#index"`
-    get '/creatures', to: "creatures#index" # same as `get "/creatures" => "creatures#index"`
+
+	root to: 'creatures#index' # same as `get "/" => "creatures#index"`
+	get '/creatures', to: "creatures#index" # same as `get "/creatures" => "creatures#index"`
+	get '/creatures/new', to: 'creatures#new'
+
 end
 
